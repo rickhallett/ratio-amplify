e581c87dd068128a01a739360261185674f9c660
Rick Hallett <rick.hallett@brandwatch.com>
2023-09-01 10:53:15 +0100

    test remote CORS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import AWS from 'aws-sdk';
 import './App.css';
+
+const s3 = new AWS.S3();
+
+const fetchData = async (setTableData) => {
+  const params = {
+    Bucket: 'ratio-expenses',
+    Key: 'shared_expenses.txt',
+  };
+
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const fileContent = data.Body.toString('utf-8');
+    const lines = fileContent.split('\n');
+    const headers = lines[0].split(',');
+    const records = lines.slice(1).map(line => line.split(','));
+
+    setTableData({ headers, records });
+  });
+};
+
+const MyTable = () => {
+  const [tableData, setTableData] = useState(null);
+
+  return (
+    <div>
+      <button onClick={() => fetchData(setTableData)}>Fetch Data</button>
+      {tableData && (
+        <table>
+          <thead>
+            <tr>
+              {tableData.headers.map((header, index) => (
+                <th key={index}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.records.map((record, index) => (
+              <tr key={index}>
+                {record.map((cell, cellIndex) => (
+                  <td key={cellIndex}>{cell}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyTable />
     </div>
   );
 }
